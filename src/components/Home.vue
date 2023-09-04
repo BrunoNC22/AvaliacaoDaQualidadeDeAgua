4586059880791efd2a1ce6b420ad37480476cb34
BrunoNC22 <thebegone2@gmail.com>
2023-09-04 18:38:05 -0300

    feat: add sheet.js

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,16 @@
 <script setup>
+import graficVue from './grafic.vue';
 </script>
-
 
 <template>
     <h1>Controle de temperatura utilizando logica fuzzy</h1>
+    <div class="grafico">
+        <graficVue></graficVue>
+    </div>
 </template>
+<style>
+.grafico {
+    width: 500px;
+    height: 500px;
+}
+</style>
