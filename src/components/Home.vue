59eaa16d3126bb866add56ceaa1fc37297d9b598
BrunoNC22 <thebegone2@gmail.com>
2023-09-16 19:47:04 -0300

    fix: Home.vue update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,17 @@
 <script setup>
 import graficVue from './grafic.vue';
+import grafic2 from './grafic2.vue';
 </script>
 
 <template>
     <h1>Controle de temperatura utilizando logica fuzzy</h1>
     <div class="grafico">
-        <graficVue></graficVue>
+        <grafic2></grafic2>
     </div>
 </template>
 <style>
 .grafico {
-    width: 500px;
+    width: 1000px;
     height: 500px;
 }
 </style>
