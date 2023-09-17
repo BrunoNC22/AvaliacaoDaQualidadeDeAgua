96ee315964abbc9fc142ab4e3b0d72f91ea8ff0c
BrunoNC22 <thebegone2@gmail.com>
2023-09-17 13:38:27 -0300

    feat: Home.vue update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,174 @@
 <script setup>
-import GraficoCorAparenteVue from './graficos/GraficoCorAparente.vue';
+import { onMounted } from 'vue'
+onMounted(() => {
+  var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar-example'
+  })
+})
 </script>
 
 <template>
-    <h1>Controle de temperatura utilizando logica fuzzy</h1>
-    <div class="grafico">
-        <GraficoCorAparenteVue></GraficoCorAparenteVue>
+  <body>
+    <div class="container-personalizado">
+      <div class="row conteudo">
+        <div class="col-3">
+          <nav id="navbar-example3"
+            class="navbar navbar-light bg-light flex-column align-items-start justify-content-start p-3">
+            <a class="navbar-brand" href="#titulo">Inicio</a>
+            <nav class="nav nav-pills flex-column">
+              <a class="nav-link" href="#item-1">Introdução</a>
+              <nav class="nav nav-pills flex-column">
+                <a class="nav-link ms-3 my-1" href="#item-1-1">Objetivo</a>
+              </nav>
+              <a class="nav-link" href="#item-2">Aplicação do SBRF na Qualidade da Água</a>
+              <nav class="nav nav-pills flex-column">
+                <a class="nav-link ms-3 my-1" href="#item-2-1">Variáveis de Entrada</a>
+                <a class="nav-link ms-3 my-1" href="#item-2-2">Variável de Saída</a>
+              </nav>
+              <a class="nav-link" href="#item-3">A Base de regras</a>
+              <nav class="nav nav-pills flex-column">
+                <a class="nav-link ms-3 my-1" href="#item-3-1">Item 3-1</a>
+                <a class="nav-link ms-3 my-1" href="#item-3-2">Item 3-2</a>
+              </nav>
+            </nav>
+          </nav>
+        </div>
+        <div data-bs-spy="scroll" data-bs-target="#navbar-example3" data-bs-offset="0" tabindex="0" class="col scrollSpy">
+          <h1 id="titulo" class="py-5 text-center">Sistema baseado em regras fuzzy para avaliação da qualidade da água
+          </h1>
+          <h4 id="item-1">Introdução</h4>
+          <p>Segundo a <b>ANA (Agencia Nacional de Aguas)</b>, um dos maiores problemas deste século e das futuras
+            gerações será
+            manter a qualidade da água potável, elemento essencial para a vida humana.
+            Um relatório da ANA apresentado em 2017 mostra um quadro preocupante da disponibilidade e do uso dos recursos
+            hídricos em todo território brasileiro. De acordo com este relatório, se nada for feito nestes próximos anos,
+            cerca de 55% dos municípios poderão sofrer com a escassez dos recursos ainda neste século. O informativo da
+            ANA destaca que a qualidade da água é muito pior nas regiões de grande concentração populacional. Além disso,
+            para a ANA os desafios da gestão pública dos recursos hídricos são grandes e tem que estar focados
+            essencialmente em dois quesitos: tratamento adequado do esgoto urbano e investimentos em sistemas de captação,
+            tratamento, projetos educacionais sobre o uso racional e distribuição de água.
+          </p>
+          <div class="img-box">
+            <img class="ana-logo" src="../../public/Logotipo_ANA_-_Agência_Nacional_de_Águas_e_Saneamento_Básico.png"
+              alt="ANA - Agência Nacional de Águas e Saneamento Básico">
+          </div>
+          <br>
+          <h5 id="item-1-1">Objetivo</h5>
+          <p>Com o objetivo de aferir a qualidade da água, a Agência Nacional de Águas (ANA) propõe o uso do Índice de
+            Qualidade das Águas (IQA), que atualmente é o principal indicador qualitativo utilizado no país. O IQA foi
+            desenvolvido para avaliar a qualidade da água para o consumo humano após o tratamento convencional. A
+            avaliação do IQA é realizada com base nos seguintes parâmetros: temperatura da água, pH, oxigênio dissolvido,
+            resíduo total, demanda bioquímica de oxigênio, coliformes termotolerantes, nitrogênio total, fósforo total e
+            turbidez.</p>
+          <p>Atendendo ao Decreto Federal nº 2.914/2011, a empresa Águas Guariroba, responsável pelo abastecimento de água
+            e tratamento do esgoto, disponibiliza informações sobre a qualidade da água consumida na cidade de Campo
+            Grande, no Estado de Mato Grosso do Sul, para todos os seus clientes. Essas informações são divulgadas
+            semestralmente e estão disponíveis em uma página da empresa na internet e em todos os postos de atendimento ao
+            cliente. A empresa Águas Guariroba, seguindo a política de qualidade da água, é responsável por aplicar as
+            normas reguladoras que qualificam a água para o consumo humano no município.
+          </p>
+          <p>O objetivo deste trabalho é desenvolver um modelo <b>SBRF (Sistema Baseado em Regras Fuzzy)</b> para avaliar
+            a
+            qualidade da água utilizada para consumo humano em Campo Grande, MS. Serão abordados três aspectos físicos e
+            químicos importantes que justificam a potabilidade da água: <b>Cor Aparente, pH e Turbidez</b>.</p>
+          <br>
+          <br>
+          <h4 id="item-2">Aplicação do SBRF na Qualidade da Água</h4>
+          <p>Para a aplicação de um SBRF na análise da qualidade da água, foram escolhidas as variáveis seguindo a
+            política de qualidade da água determinada pela empresa Águas Guariroba, as quais garantem a potabilidade da
+            água. Os dados são encontrados em <a href="http://www.aguasguariroba.com.br" target="_blank"
+              rel="noopener noreferrer">http://www.aguasguariroba.com.br</a>.
+          </p>
+          <br>
+          <h5 id="item-2-1">Variáveis de Entrada</h5>
+          <p>Assim, as variáveis de entrada escolhidas para o SBRF foram:</p>
+          <p>1. Cor Aparente (medida em UH - Unidade Hazen) <br>
+            2. pH (potencial Hidrogeniônico), ou seja, concentração de íons de Hidrogênio<br>
+            3. Turbidez (causada pela presença de substâncias suspensas e coloidais, determinada pela quantidade de luz
+            dispersada quando ela passa através de uma amostra e é medida em UT - Unidade de Turbidez, ou seja, unidades
+            de cor).</p>
+          <p>Além dessas três variáveis abordadas, outras poderiam ser utilizadas, tais como: cloro ativo, Escherichia
+            coli, nível de flúor, quantidade de coliformes fecais e totais.</p>
+          <h5 id="item-2-2">Variável de saída</h5>
+          <p>A variável de saída é a <b>Potabilidade da água</b>, com os termos linguísticos: <b>boa, adequada e
+              inadequada para
+              o consumo</b>.</p>
+          <br>
+          <br>
+          <h4 id="item-3">A Base de regras</h4>
+          <p>A base de regras de um sistema fuzzy estabelece uma relação entre as variáveis independentes. Para uma melhor
+            descrição dessas regras, foram organizadas três tabelas de dupla entrada, fixando o pH nas linhas e a Turbidez
+            nas colunas e variando a Cor Aparente em cada tabela. Como resultado, temos as 36 combinações que determinam
+            as condições da variável dependente Qualidade da Água.</p>
+          <h5 id="item-3-1">Item 3-1</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque cumque beatae eaque excepturi
+            distinctio unde. A quos blanditiis temporibus natus aut dolor, soluta possimus consequatur beatae in
+            aperiam facere, ut, excepturi quae reprehenderit quo? Totam pariatur quidem voluptas vel. Inventore
+            fuga labore tenetur illo nobis nam tempore dolorum accusantium aperiam aliquid sequi error, at
+            corporis possimus totam sit, minima accusamus molestiae facere culpa? Id, neque aliquid numquam
+            cumque ullam laboriosam quasi debitis. Corrupti, vero! Fuga laudantium est quaerat dignissimos vel
+            placeat voluptatum temporibus ex aspernatur, perspiciatis iure nostrum numquam ratione adipisci
+            fugit dolores dolorum deleniti officiis. Ad in nobis et.</p>
+          <h5 id="item-3-2">Item 3-2</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque cumque beatae eaque excepturi
+            distinctio unde. A quos blanditiis temporibus natus aut dolor, soluta possimus consequatur beatae in
+            aperiam facere, ut, excepturi quae reprehenderit quo? Totam pariatur quidem voluptas vel. Inventore
+            fuga labore tenetur illo nobis nam tempore dolorum accusantium aperiam aliquid sequi error, at
+            corporis possimus totam sit, minima accusamus molestiae facere culpa? Id, neque aliquid numquam
+            cumque ullam laboriosam quasi debitis. Corrupti, vero! Fuga laudantium est quaerat dignissimos vel
+            placeat voluptatum temporibus ex aspernatur, perspiciatis iure nostrum numquam ratione adipisci
+            fugit dolores dolorum deleniti officiis. Ad in nobis et.</p>
+        </div>
+      </div>
     </div>
+  </body>
 </template>
 <style>
-.grafico {
-    width: 500px;
-    height: 250px;
+body {
+  background-color: #fff;
+  padding: 0px;
+  margin: 0px;
+
 }
-</style>
+
+.container {
+  width: 100%;
+  margin: 0px;
+  padding: 0px;
+  border: 0px;
+}
+
+.conteudo {
+  width: 100%;
+}
+
+.img-box {
+  display: flex;
+  justify-content: center;
+}
+
+.ana-logo {
+  width: 450px;
+  height: 225px;
+}
+
+.scrollSpy {
+  width: 100%;
+  padding-right: -5%;
+  overflow-y: auto;
+  height: 650px;
+  position: relative;
+}
+
+h4 {
+  font-size: 35px;
+}
+
+h5 {
+  font-size: 30px;
+}
+
+#navbar-example3 {
+  height: 100%;
+
+}</style>
